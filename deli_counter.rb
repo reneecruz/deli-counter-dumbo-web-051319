38b8c31dc16dc8bff1262b70af6bbb690fbecd9a
Renée Cruz <reneebasscruz@gmail.com>
2019-05-03 02:49:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+#def assign_rooms(attendees)
+#  attendees.each_with_index.map do |attendee, index|
+#    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+#  end
+#end
+
+katz_deli = []
+def line(katz_deli) 
+    if (katz_deli.length == 0) 
+        puts "The line is currently empty."
+    end
+    
+    katz_deli.each_with_index do |element,i|
+      puts "The line is currently: #{i+1}. #{element}"
+    end
+    
+end
+
+def take_a_number(katz_deli, name) 
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving(katz_deli) 
+  if (!katz_deli.length == 0) 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift()}."
+  end
+end
