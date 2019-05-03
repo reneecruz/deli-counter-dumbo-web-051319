677be5c6a61ec45f20a4bf41240ddadae6373d73
Renée Cruz <reneebasscruz@gmail.com>
2019-05-03 03:07:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name) 
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
 end
 
 def now_serving(katz_deli) 
