32028342cbe29276dd0492054e3752fc6fddc1c6
Renée Cruz <reneebasscruz@gmail.com>
2019-05-03 03:42:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   
     current_line = ""
     katz_deli.each_with_index do |element,i|
-    current_line.push("The line is currently: #{i+1}. #{element}")
+    current_line << "The line is currently: #{i+1}. #{element}")
     end
     current_line
 end
