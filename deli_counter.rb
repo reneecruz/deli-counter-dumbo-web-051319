5608fc7a6775ba9f74f7ab9ca2f510c07609d5bf
Renée Cruz <reneebasscruz@gmail.com>
2019-05-03 03:27:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |element,i|
     current_line = "The line is currently: #{i+1}. #{element}"
     end
-    return current_line
+    puts current_line
 end
 
 def take_a_number(katz_deli, name) 
